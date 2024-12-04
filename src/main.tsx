c501ef483dcce56b0922769793a80c14c2c220e9
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-12-04 23:04:35 +0530

    reactprime setup and configuratiopn done

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { PrimeReactProvider } from 'primereact/api';
+import './index.css';
+import App from './App.tsx';
+
+const value = {
+  ripple: true,
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <PrimeReactProvider value={value}>
+      <App />
+    </PrimeReactProvider>
+  </StrictMode>
+);
